077fbb6d182438acff89bd1f61599f83513f2d8c
CooooolFrog <zuliangwanghust@gmail.com>
2024-03-29 15:29:14 +0800

    rename package

--- horaedb-example/src/test/java/org/apache/horaedb/ReadmeTest.java
+++ horaedb-example/src/test/java/org/apache/horaedb/ReadmeTest.java
@@ -3,9 +3,7 @@
  */
 package org.apache.horaedb;
 
-import io.ceresdb.models.*;
 import org.apache.horaedb.models.*;
-import org.apache.models.*;
 import org.apache.horaedb.options.HoraeDBOptions;
 import org.apache.horaedb.util.StreamWriteBuf;
 
@@ -25,15 +23,15 @@ public class ReadmeTest {
     @Ignore
     @Test
     public void readmeTest() throws ExecutionException, InterruptedException {
-        final HoraeDBOptions opts = HoraeDBOptions.newBuilder("127.0.0.1", 8831, RouteMode.DIRECT) // CeresDB default grpc port 8831
+        final HoraeDBOptions opts = HoraeDBOptions.newBuilder("127.0.0.1", 8831,
+                        RouteMode.DIRECT) // CeresDB default grpc port 8831
                 .database("public") // use database public
                 // maximum retry times when write fails
                 // (only some error codes will be retried, such as the routing table failure)
                 .writeMaxRetries(1)
                 // maximum retry times when read fails
                 // (only some error codes will be retried, such as the routing table failure)
-                .readMaxRetries(1)
-                .build();
+                .readMaxRetries(1).build();
 
         final HoraeDBClient client = new HoraeDBClient();
         if (!client.init(opts)) {
@@ -41,13 +39,8 @@ public class ReadmeTest {
         }
 
         // Create table manually, creating table schema ahead of data ingestion is not required
-        String createTableSql = "CREATE TABLE IF NOT EXISTS machine_table(" +                                                                                              "ts TIMESTAMP NOT NULL," + //
-                "ts TIMESTAMP NOT NULL," +
-                "city STRING TAG NOT NULL," +
-                "ip STRING TAG NOT NULL," +
-                "cpu DOUBLE NULL," +
-                "mem DOUBLE NULL," +
-                "TIMESTAMP KEY(ts)" + // timestamp column must be specified
+        String createTableSql = "CREATE TABLE IF NOT EXISTS machine_table(" + "ts TIMESTAMP NOT NULL," + //
+                "ts TIMESTAMP NOT NULL," + "city STRING TAG NOT NULL," + "ip STRING TAG NOT NULL," + "cpu DOUBLE NULL," + "mem DOUBLE NULL," + "TIMESTAMP KEY(ts)" + // timestamp column must be specified
                 ") ENGINE=Analytic";
 
         Result<SqlQueryOk, Err> createResult = client.sqlQuery(new SqlQueryRequest(createTableSql)).get();
@@ -56,27 +49,15 @@ public class ReadmeTest {
         }
 
         final long timestamp = System.currentTimeMillis();
-        Point point1 = Point.newPointBuilder("machine_table")
-                    .setTimestamp(timestamp)
-                    .addTag("city", "Singapore")
-                    .addTag("ip", "10.0.0.1")
-                    .addField("cpu", Value.withDouble(0.23))
-                    .addField("mem", Value.withDouble(0.55))
-                    .build();
-        Point point2 = Point.newPointBuilder("machine_table")
-                    .setTimestamp(timestamp+1000)
-                    .addTag("city", "Singapore")
-                    .addTag("ip", "10.0.0.1")
-                    .addField("cpu", Value.withDouble(0.25))
-                    .addField("mem", Value.withDouble(0.56))
-                    .build();
-        Point point3 = Point.newPointBuilder("machine_table")
-                    .setTimestamp(timestamp+1000)
-                    .addTag("city", "Shanghai")
-                    .addTag("ip", "10.0.0.2")
-                    .addField("cpu", Value.withDouble(0.21))
-                    .addField("mem", Value.withDouble(0.52))
-                    .build();
+        Point point1 = Point.newPointBuilder("machine_table").setTimestamp(timestamp).addTag("city", "Singapore")
+                .addTag("ip", "10.0.0.1").addField("cpu", Value.withDouble(0.23))
+                .addField("mem", Value.withDouble(0.55)).build();
+        Point point2 = Point.newPointBuilder("machine_table").setTimestamp(timestamp + 1000).addTag("city", "Singapore")
+                .addTag("ip", "10.0.0.1").addField("cpu", Value.withDouble(0.25))
+                .addField("mem", Value.withDouble(0.56)).build();
+        Point point3 = Point.newPointBuilder("machine_table").setTimestamp(timestamp + 1000).addTag("city", "Shanghai")
+                .addTag("ip", "10.0.0.2").addField("cpu", Value.withDouble(0.21))
+                .addField("mem", Value.withDouble(0.52)).build();
         List<Point> pointList = Arrays.asList(point1, point2, point3);
 
         final CompletableFuture<Result<WriteOk, Err>> wf = client.write(new WriteRequest(pointList));
@@ -89,10 +70,9 @@ public class ReadmeTest {
         Assert.assertEquals(0, writeResult.getOk().getFailed());
         Assert.assertEquals(0, writeResult.mapOr(-1, WriteOk::getFailed).intValue());
 
-        final SqlQueryRequest queryRequest = SqlQueryRequest.newBuilder()
-                .forTables("machine_table") // table name is optional. If not provided, SQL parser will parse the `sql` to get the table name and do the routing automaticly
-                .sql("select * from machine_table where ts >= %d", timestamp)
-                .build();
+        final SqlQueryRequest queryRequest = SqlQueryRequest.newBuilder().forTables(
+                        "machine_table") // table name is optional. If not provided, SQL parser will parse the `sql` to get the table name and do the routing automaticly
+                .sql("select * from machine_table where ts >= %d", timestamp).build();
         final CompletableFuture<Result<SqlQueryOk, Err>> qf = client.sqlQuery(queryRequest);
         // here the `future.get` is just for demonstration, a better async programming practice would be using the CompletableFuture API
         final Result<SqlQueryOk, Err> queryResult = qf.get();
@@ -111,21 +91,18 @@ public class ReadmeTest {
         long t = start;
         final StreamWriteBuf<Point, WriteOk> writeBuf = client.streamWrite("machine_table");
         for (int i = 0; i < 1000; i++) {
-            final Point streamData = Point.newPointBuilder("machine_table")
-                        .setTimestamp(t)
-                        .addTag("city", "Beijing")
-                        .addTag("ip", "10.0.0.3")
-                        .addField("cpu", Value.withDouble(0.42))
-                        .addField("mem", Value.withDouble(0.67))
-                    .build();
+            final Point streamData = Point.newPointBuilder("machine_table").setTimestamp(t).addTag("city", "Beijing")
+                    .addTag("ip", "10.0.0.3").addField("cpu", Value.withDouble(0.42))
+                    .addField("mem", Value.withDouble(0.67)).build();
             writeBuf.writeAndFlush(Collections.singletonList(streamData));
-            t = t+1;
+            t = t + 1;
         }
         final CompletableFuture<WriteOk> writeOk = writeBuf.completed();
         Assert.assertEquals(1000, writeOk.join().getSuccess());
 
         final SqlQueryRequest streamQuerySql = SqlQueryRequest.newBuilder()
-                .sql("select * from %s where city = '%s' and ts >= %d and ts < %d", "machine_table", "Beijing", start, t).build();
+                .sql("select * from %s where city = '%s' and ts >= %d and ts < %d", "machine_table", "Beijing", start,
+                        t).build();
         final Result<SqlQueryOk, Err> streamQueryResult = client.sqlQuery(streamQuerySql).get();
         Assert.assertTrue(streamQueryResult.isOk());
         Assert.assertEquals(1000, streamQueryResult.getOk().getRowCount());
